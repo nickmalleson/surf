d8d14f2153f60f46d357a62f55d495404ad15449
Nick Malleson <n.s.malleson@leeds.ac.uk>
2016-03-08 17:41:11 +0000

    Created run script for SimBurglarMason

---
+++ abm/surf-mason/SimBurglarMason/run.sh
@@ -0,0 +1,15 @@
+# Script to start the SimBurglar model
+
+# Set up the classpath, making sure that the model libraries come first
+export CLASSPATH=\
+${PWD}/lib/*:\
+${PWD}/../resources/jts-1.13.jar:\
+${PWD}/../resources/libraries/*:\
+${PWD}/../mason/mason/jar/mason.19.jar:\
+${PWD}/../resources/geotools/*:\
+${PWD}/../resources/jts-1.8.0/lib/*:\
+${PWD}/../geomason-1.5/geomason.1.5.jar:\
+${PWD}/build/classes/
+
+# Run java, passing all command line arguments
+java -server simburglar.SimBurglar "$@"
