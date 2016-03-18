c8081ab14211b0f848d71fa4d9888d8b52cf0fe0
Nick Malleson <n.s.malleson@leeds.ac.uk>
2016-03-18 18:01:12 +0000

    finished gui

--- abm/surf-abm/src/surf/abm/SurfABMWithUI.scala
+++ abm/surf-abm/src/surf/abm/SurfABMWithUI.scala
@@ -7,7 +7,7 @@ import org.apache.log4j.Logger
 import sim.display.{Console, GUIState, Display2D, Controller}
 import sim.portrayal.geo.{GeomPortrayal, GeomVectorFieldPortrayal}
 import sim.portrayal.{DrawInfo2D, SimplePortrayal2D}
-import sim.portrayal.simple.LabelledPortrayal2D
+import sim.portrayal.simple.{OvalPortrayal2D, LabelledPortrayal2D}
 import sim.util.geo.MasonGeometry
 
 /**
@@ -21,16 +21,16 @@ class SurfABMWithUI extends GUIState (new SurfABM(System.currentTimeMillis())) {
 
   private var displayFrame: JFrame = null
   // Portrayals are used for visualising objects in fields
-  private val walkwaysPortrayal: GeomVectorFieldPortrayal = new GeomVectorFieldPortrayal()
+  private val roadsPortrayal: GeomVectorFieldPortrayal = new GeomVectorFieldPortrayal()
   private val buildingPortrayal: GeomVectorFieldPortrayal = new GeomVectorFieldPortrayal()
   private val agentPortrayal: GeomVectorFieldPortrayal = new GeomVectorFieldPortrayal()
 
   override def init(controller : Controller ) : Unit = {
     super.init(controller);
 
-    display.attach(walkwaysPortrayal, "Walkways", true);
+    display.attach(roadsPortrayal, "Walkways", true);
     display.attach(buildingPortrayal, "Buildings", true);
-    //        display.attach(roadsPortrayal, "Roads", true);
+    //display.attach(roadsPortrayal, "Roads", true);
     display.attach(agentPortrayal, "Agents", true);
 
     displayFrame = display.createFrame();
@@ -43,23 +43,21 @@ class SurfABMWithUI extends GUIState (new SurfABM(System.currentTimeMillis())) {
     super.start();
     //val world : SurfABM = super.state.asInstanceOf[SurfABM]
 
-    //walkwaysPortrayal.setField(world.roads);
-    walkwaysPortrayal.setField(SurfABM.roads);
-    walkwaysPortrayal.setPortrayalForAll(new GeomPortrayal(Color.CYAN, true));
+    roadsPortrayal.setField(SurfABM.roads)
+    roadsPortrayal.setPortrayalForAll(new GeomPortrayal(Color.GRAY, true));
 
-    //buildingPortrayal.setField(world.buildings);
-    buildingPortrayal.setField(SurfABM.buildings);
-    val b = new BuildingLabelPortrayal(new GeomPortrayal(Color.DARK_GRAY, true), Color.BLUE);
-    buildingPortrayal.setPortrayalForAll(b);
+    buildingPortrayal.setField(SurfABM.buildings)
+    buildingPortrayal.setPortrayalForAll(
+      new BuildingLabelPortrayal(new GeomPortrayal(Color.BLUE, true), Color.BLACK))
 
-    //agentPortrayal.setField(world.agents);
-    agentPortrayal.setField(SurfABM.agents);
-    agentPortrayal.setPortrayalForAll(new GeomPortrayal(Color.RED, 10.0, true));
+    agentPortrayal.setField(SurfABM.agents)
+    //agentPortrayal.setPortrayalForAll(new GeomPortrayal(Color.RED, 10.0, true))
+    agentPortrayal.setPortrayalForAll(new OvalPortrayal2D(Color.RED, 10.0, true))
 
-    display.reset();
-    display.setBackdrop(Color.WHITE);
+    display.reset()
+    display.setBackdrop(Color.WHITE)
 
-    display.repaint();
+    display.repaint()
   }
 
 }
@@ -71,7 +69,7 @@ object SurfABMWithUI {
   def main(args: Array[String]): Unit = {
 
     try {
-      var worldGUI = new SurfABMWithUI()
+      val worldGUI = new SurfABMWithUI()
 
       val console = new Console(worldGUI)
       console.setVisible(true)
@@ -86,23 +84,18 @@ object SurfABMWithUI {
 }
 
 
+/**
+  * A special portrayal for buildings that provides labels for the buildings in the GUI.
+  */
 @SerialVersionUID(1L)
 class BuildingLabelPortrayal(child : SimplePortrayal2D, paint : Paint)
   extends LabelledPortrayal2D (child, null, paint, true) {
 
   override def getLabel(o: AnyRef, info: DrawInfo2D): String = {
-    /*if (o.isInstanceOf[MasonGeometry]) {
-      val mg: MasonGeometry = o.asInstanceOf[MasonGeometry]
-      return mg.getStringAttribute("NAME")
-    }*/
-
     // cast the object to a MasonGemoetry using pattern matching and return the
     // building name. Or return 'no name' if the object is not a building
     o match {
       case x: MasonGeometry => x.getStringAttribute(FIELDS.BUILDINGS_NAME.toString)
-        //val mg: MasonGeometry = o.asInstanceOf[MasonGeometry]
-        //mg.getStringAttribute("NAME")
-
       case _ => {
         BuildingLabelPortrayal.LOG.warn("Cannot find a label for a building", new Exception())
         "No Building Name" // no label to return, send "No Building Name" back
@@ -114,7 +107,5 @@ class BuildingLabelPortrayal(child : SimplePortrayal2D, paint : Paint)
 
 object BuildingLabelPortrayal {
   private val LOG: Logger = Logger.getLogger(this.getClass)
-
-
 }
 
