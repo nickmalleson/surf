895aaedbe7da406d5344de9d7a982f62981bc276
Nick Malleson <n.s.malleson@leeds.ac.uk>
2016-04-16 16:20:25 +0100

    IMplementing script that checks connectedness of road network

--- abm/surf-abm/src/surf/abm/surfutil/CheckConnectedNetwork.scala
+++ abm/surf-abm/src/surf/abm/surfutil/CheckConnectedNetwork.scala
@@ -1,11 +1,12 @@
 package surf.abm.surfutil
 
-import java.io.File
-import java.net.URL
+import java.io.{IOException, File}
 
+import com.vividsolutions.jts.planargraph.Node
 import org.apache.log4j.Logger
 import sim.field.geo.GeomVectorField
 import sim.io.geo.ShapeFileImporter
+import sim.util.geo.GeomPlanarGraph
 import surf.abm.SurfABM
 
 /**
@@ -20,25 +21,79 @@ case object CheckConnectedNetwork {
 
   def main(args: Array[String]): Unit = {
 
-      if (args.length != 0) {
-        this.LOG.error("The program expects one command-line argument; an input roads shapefile.")
-        this.LOG.error("Usage: scala surf.abm.surfutil.CheckConnectedNetwork [roads_file.shp]")
-        throw new Exception("Incorrect number of command line arguments: %d".format(args.length))
-      }
-      val roadsFileName = args(0)
-      this.LOG.info("Checking that road network in '%s' is connected".format(roadsFileName))
-      val roadsURI= new File(roadsFileName).toURI().toURL()
-      val roadsField = new GeomVectorField(SurfABM.WIDTH, SurfABM.HEIGHT)
-      ShapeFileImporter.read(roadsURI, roadsField)
+    if (args.length != 1) {
+      this.LOG.error("The program expects one command-line argument; an input roads shapefile.")
+      this.LOG.error("Usage: scala surf.abm.surfutil.CheckConnectedNetwork [roads_file.shp]")
+      throw new Exception("Incorrect number of command line arguments: %d".format(args.length))
+    }
 
-
-    /*catch {
-      case e: Exception => {
-        this.LOG.error("Exception thrown in main loop", e)
-        throw e
-      }
-    }*/
+    this.run(args(0))
 
   } // main()
+
+  def run ( filename: String): Unit = {
+    val roadsField = this.readRoadsFile(filename)
+    val roadsNetwork = this.createNetwork(roadsField)
+    val disconnected = this.checkDisconnected(roadsNetwork)
+  }
+
+  def readRoadsFile(filename: String) : GeomVectorField = {
+    val f = new File(filename)
+    if ( !f.exists() || !f.canRead) {
+      throw new IOException("Cannot read from file %s".format(filename))
+    }
+    this.LOG.info("Checking that road network in '%s' is connected".format(filename))
+    val roadsField = new GeomVectorField(SurfABM.WIDTH, SurfABM.HEIGHT)
+    ShapeFileImporter.read(new File(filename).toURI().toURL(), roadsField)
+    this.LOG.info("Read %d roads".format(roadsField.getGeometries().size()))
+    return roadsField
+  }
+
+  def createNetwork(field: GeomVectorField) : GeomPlanarGraph = {
+    val network = new GeomPlanarGraph()
+    network.createFromGeomField(field)
+    return network
+  }
+
+  /**
+    * Checks whether the input disconnected.
+ *
+    * @param network
+    * @return True if disconnected, false otherwise
+    */
+  def checkDisconnected(network: GeomPlanarGraph): Boolean = {
+
+    this.LOG.info("Checking if disconnected")
+
+    // Choose node zero to start with.
+    val it : java.util.Iterator[Node] =
+      network.getNodes().iterator().asInstanceOf[java.util.Iterator[Node]]
+    val start : Node = it.next()
+
+    val connected = scala.collection.mutable.HashSet[Node](start)
+
+    // XXXX COULD USE FOLLOWING TO REATE disconnected HASHSET from all nodes?
+
+    val a = ArrayBuffer[AnyRef]()
+    for(i <- 1 until bag.size()) {
+      a += bag.get(i)
+    }
+    return List(a:_*)
+
+    val disconnected = scala.collection.mutable.HashSet[Node](
+      network.getNodes(): _*
+    )
+    val dis
+    while (it.hasNext()) {
+      // XXXX HERE how to get a path to the next node
+      // THIS IS DIFFICULT. PROBABLY BETTER TO RECURSE USING getEdgesIn()
+      // and move each junction that we touch to the 'connected' list
+      start.
+      network.getNetwork().XXXX
+    }
+
+    return false
+  }
+
 
 }
--- abm/surf-abm/test/surf/abm/surfutil/CheckConnectedNetworkSpec.scala
+++ abm/surf-abm/test/surf/abm/surfutil/CheckConnectedNetworkSpec.scala
@@ -1,9 +1,9 @@
 package surf.abm.surfutil
 
-import java.io.FileNotFoundException
+import java.io.{IOException, FileNotFoundException}
 
 import org.apache.log4j.Logger
-import surf.abm.SurfABM
+import sim.field.geo.GeomVectorField
 import surf.abm.tests.UnitSpec
 
 /**
@@ -26,9 +26,44 @@ class CheckConnectedNetworkSpec extends UnitSpec {
   }
 
   it should "throw an error if the roads file cannot be found" in {
-    a [FileNotFoundException] should be thrownBy {
+    an [IOException] should be thrownBy {
       CheckConnectedNetwork.main(Array[String]("not_a_roads_file"))
     }
+  }
+
+  it should "return a GeomVectorField from a valid file" in {
+    val field = CheckConnectedNetwork.readRoadsFile("./data/leeds/roads_disconnected.shp")
+    field.getClass() should be (new GeomVectorField(1,1).getClass())
+
+
+  }
+  it should "read more than 8614 roads from the leeds_disconnected file" in {
+    val field = CheckConnectedNetwork.readRoadsFile("./data/leeds/roads_disconnected.shp")
+    field.getGeometries().size() should be (8614)
+  }
+
+  it should "return a network with 8614 edges" in {
+    val network =CheckConnectedNetwork.createNetwork(
+      CheckConnectedNetwork.readRoadsFile("./data/leeds/roads_disconnected.shp")
+    )
+    network.getEdges().size() should be (8614)
+
+  }
+
+  it should "return a network with 6749 nodes" in {
+    val network =CheckConnectedNetwork.createNetwork(
+      CheckConnectedNetwork.readRoadsFile("./data/leeds/roads_disconnected.shp")
+    )
+    network.getNodes().size() should be (6749)
+
+  }
+
+
+  it should "report find tha the leeds_disconnected file is disconnected" in {
+    val field = CheckConnectedNetwork.readRoadsFile("./data/leeds/roads_disconnected.shp")
+    val network = CheckConnectedNetwork.createNetwork(field)
+    val disconnect = CheckConnectedNetwork.checkDisconnected(network)
+    disconnect should be (true)
   }
 
 
