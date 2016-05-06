976004ac7cb17d3c35f2fe5caa89068c25a9b81f
Nick Malleson <n.s.malleson@leeds.ac.uk>
2016-05-06 10:12:20 +0100

    have begun implementing behavioural framework

---
+++ abm/surf-abm/src/surf/abm/agents/abbf/package.scala
@@ -0,0 +1,11 @@
+package surf.abm.agents
+
+/**
+  * Activity-Based Behavioural Framework (abbf)
+  *
+  * ==Overview==
+  * Activity-Based Behavioural Framework is a framework for controlling agent behaviour through the use of
+  * competing [[surf.abm.agents.abbf.Activity]]s. The most intense of these drives the behaviour of the agent.
+  *
+  */
+package object abbf {}
---
+++ abm/surf-abm/src/surf/abm/agents/package.scala
@@ -0,0 +1,19 @@
+package surf.abm
+
+/**
+  * Classes to represent agents
+  *
+  * ==Overview==
+  *
+  * The base class for all agents is [[surf.abm.agents.Agent]], which provides the basic parameters and methods required
+  * by all agents.
+  *
+  * The [[surf.abm.agents.UrbanAgent]] class extends the basic agent functionality by adding the ability of agents
+  * to move around a road network
+  *
+  * [[surf.abm.agents.RandomRoadAgent]] is a concrete implementation of an [[surf.abm.agents.UrbanAgent]] that
+  * moves randomly from home to a randomly chosen [[surf.abm.environment.Building]] and back again.
+  */
+package object agents {
+
+}
