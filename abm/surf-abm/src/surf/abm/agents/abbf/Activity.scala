86be24eeb38b07db566043c4b6dc062dcd5c9e2c
Nick Malleson <n.s.malleson@leeds.ac.uk>
2016-05-08 20:51:08 +0100

    continuing to implement tests of TimeProfile

--- abm/surf-abm/src/surf/abm/agents/abbf/Activity.scala
+++ abm/surf-abm/src/surf/abm/agents/abbf/Activity.scala
@@ -31,6 +31,7 @@ object Activity {
   *                val d = Array( (0,0), (9,0.8), (14,1.0), (17,0.8) )
   *                val t = TimeProfile(d)
   *                }}}
+  *                If only one (time,intensity) pair is provided, then we assume constant intensity.
   *                Note: all times must (obviously) be in the range [0,24) and should be in
   *                ascending order (to make my life easier).
   *
@@ -46,11 +47,79 @@ case class TimeProfile (val profile: Array[(Double,Double)] ) {
 
   /**
     * Calculate the intensity at time t
+    *
     * @param t The time period to check. Must be in range [0,24)
     * @return The intensity, calculates as the linear interpolation between the nearest time points.
     */
   def calcIntensity (t:Double) : Double = {
-  return 1.0
+
+    // Cannot calculate intensities outside the range [0-24)
+    if (t < 0 || t >= 24) {
+      throw new IllegalArgumentException(s"Input time ($t) needs to be in the range [0-24)")
+    }
+
+    // If there is only one reference point, then assume a constant intensity
+    if (this.times.size == 1) {
+      return this.intensities(0)
+    }
+
+    // If the exact time exists, just return the intensity there
+    val index = this.times.indexOf(t)
+    if (index != -1)
+      return this.intensities(index)
+
+
+    // Find the two nearest time points, t0 and t1 and their indices i0 and 01
+    var t0, t1 = -1d // time0 and time1
+    var i0, i1 = -1d // intensity0 and intensity1
+    for ((time:Double,intensity:Double) <- this.times.zip(this.intensities)) {
+      if (t0 <= time) {
+        t0 = time
+        i0 = intensity
+      }
+      if (t1 >= t) {
+        t1 = time
+        i1 = intensity
+      }
+    }
+    assert(t0 != t1) // If this happens then t == t1 == t2 (i.e. the exact time exists in the list) and the earlier if() should have caught it
+    if (t0 != -1d && t1 != -1d) {
+      return interpolate(t, t0, i0, t1, i1)
+    }
+
+    assert ( !( t0 == -1d && t1 == -1d ) ) // At least on of the time points should have been set
+
+    // Special case1: the first intensity actually occurs before midnight (wrap around)
+    if (t0 == -1d) {
+      t0 = this.times.last - 24 // Need to take 24 away to trick the interpolation
+      i0 = this.intensities.last
+    }
+    // Special case2: the last time occurs after midnight (wraps around)
+    if (t1 == -1d) {
+      t1 = this.times(0) + 24 // Add 24 to wrap around
+      i1 = this.intensities(0)
+    }
+    return interpolate(t, t0, i0, t1, i1)
+  }
+
+  /**
+    * Linear interpolation between the two points. Note: x1>x2 or an IllegalArgumentException is thrown
+    *
+    * @param x The number to interpolate
+    * @param x0
+    * @param y0
+    * @param x1
+    * @param y1
+    * @return
+    */
+  private def interpolate ( x:Double, x0:Double,y0:Double, x1:Double,y1:Double ) : Double = {
+    if (x1 > x0) {
+      return y0 + (y1-y0) * ( (x-x0) / (x1-x0))
+    }
+    else {
+      throw new IllegalArgumentException(s"Second input ($x1) must be greater than first input ($x0)")
+    }
+
   }
 }
 
@@ -61,6 +130,9 @@ object TimeProfile {
     * Throwns a [[java.lang.IllegalArgumentException]] otherwise.
     */
   def checkTimes(profile:  Array[(Double,Double)]) : Unit = {
+
+    // Input array cannot be empty
+    if (profile.isEmpty) throw new IllegalArgumentException("Input profile must have at least one (time,intensity) pair")
 
     // Check that all times are in the range [0,24)
     for ( (i,j) <- profile) {
--- abm/surf-abm/test/surf/abm/agents/abbf/ActivitySpec.scala
+++ abm/surf-abm/test/surf/abm/agents/abbf/ActivitySpec.scala
@@ -1,18 +1,27 @@
 package surf.abm.agents.abbf
 
+import javax.measure.quantity.LuminousIntensity
+
 import org.apache.log4j.Logger
-import org.scalatest.GivenWhenThen
+import org.scalatest.{GivenWhenThen, Matchers}
 import surf.abm.surfutil.CheckConnectedNetwork
 import surf.abm.tests.UnitSpec
 
 /**
   * Created by nick on 07/05/2016.
   */
-class ActivitySpec extends UnitSpec with GivenWhenThen {
+class ActivitySpec extends UnitSpec with GivenWhenThen with Matchers {
 
     // Tests consist of a *subject*, a *verb* (either 'should', 'must', or 'can') and the rest of the sentence
 
-    "The  checkTimes function" should "throw an illegal argument exception if any of the times are not in the range [0-24)" in {
+    "The  checkTimes function" should "throw an illegal argument exception if the input arry size is 0" in {
+      an [IllegalArgumentException] should be thrownBy {
+        TimeProfile.checkTimes(Array.empty[(Double,Double)])
+      }
+    }
+
+
+  it should "throw an illegal argument exception if any of the times are not in the range [0-24)" in {
       val i = 1.0 // the intensity
       val t = 1.0 // the time
 
@@ -54,6 +63,7 @@ class ActivitySpec extends UnitSpec with GivenWhenThen {
     }
   }
 
+
   it should "return Unit if the times are in ascending order and in the range [0-24)" in {
 
     val i = 1.0 // the intensity
@@ -77,17 +87,65 @@ class ActivitySpec extends UnitSpec with GivenWhenThen {
     }
   }
 
-  it should "return an intensity of XX" in {
+  it should "return a single intensity regardless of the input time parameter if the TimeProfile only has one data point " in {
+    val INTENSITY = 15d
+    val tp = TimeProfile(Array((5d, INTENSITY)) ) // An Array with only 1 (time,intensity) Pair
 
-    val tp = TimeProfile(Array((0d, 0d), (10d, 8.0), (13d, 14.0), (15d, 10.0), (23d, 5.0)))
-    tp.calcIntensity(9.5) should equal(XX)
+    for (t <- Seq(1d, 15d, 23d)) {
+      tp.calcIntensity(t) should equal (INTENSITY)
+    }
+
+  }
+
+  it should "return the exact intensity if the passed time period exists" in {
+    val tp = TimeProfile(Array((0d, 0d), (10d, 8.0), (13.5d, 14.0), (15d, 10.0), (23d, 5.0)))
+    tp.calcIntensity(10) should equal (8)
+    tp.calcIntensity(13.5) should equal (14)
+    tp.calcIntensity(15) should equal (10)
+
+  }
+
+  it should "interpolate correctly with constant intensities" in {
+
+    val tp = TimeProfile(Array( (10d, 5d), (13d, 5d), (15d, 5d) ) )
+    for (t <- Seq(1d, 10d, 10.5 , 13d, 23d, 23.5)) {
+      tp.calcIntensity(t) should equal (5d)
+    }
+
+  }
+
+  it should "interpolate correctly with varied intensities" in {
+
+    val tp = TimeProfile(Array((5d, 0d), (10d, 8.0), (13d, 14.0), (15d, 10.0), (23d, 5.0)))
+    //for (t <- Seq(1d, 10d, 10.5, 13d, 17d, 20d, 23.5)) {
+    //  val i = tp.calcIntensity(t)
+    //  println(s"XX t:$t  -  i:$i")
+   // }
+
+    tp.calcIntensity(1d) should be > 0d
+    tp.calcIntensity(1d) should be < 5d
+    //tp.calcIntensity(1d) should equal (XX)
+
+    tp.calcIntensity(10d) should equal (8d)
+
+    tp.calcIntensity(10.5) should be > 8d
+    tp.calcIntensity(10.5) should be < 14d
+    //tp.calcIntensity(10.5) should equal (XX)
+
+    tp.calcIntensity(13d) should equal (14d)
+
+    tp.calcIntensity(17d) should be < 10d
+    tp.calcIntensity(17d) should be > 5d
+
+    tp.calcIntensity(20d) should be < 10d
+    tp.calcIntensity(20d) should be > 5d
+    //tp.calcIntensity(23d) should equal (XX)
+
+    tp.calcIntensity(23.5) should be < 5d
+    tp.calcIntensity(23.5) should be > 0d
+     // tp.calcIntensity(23) should equal (XX)
 
   }
 
 
-}
-
-
-object ActivitySpec {
-  private val LOG: Logger = Logger.getLogger(this.getClass);
 }
