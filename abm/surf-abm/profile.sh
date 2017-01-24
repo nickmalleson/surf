391c1bf1d553ffe9195e8ea3f7487e291521b831
Nick Malleson <n.s.malleson@leeds.ac.uk>
2017-01-24 13:00:07 +0000

    minor

---
+++ abm/surf-abm/profile.sh
@@ -0,0 +1,10 @@
+env JAVA_OPTS="-Xmx4G" \
+scala -cp ../resources/mason/mason/build/:\
+../resources/mason/contrib/geomason/build/:\
+../resources/mason_resources/*:\
+./lib/*:\
+./build/:\
+../resources/config/classes/\
+ surf.abm.main.SurfABM\
+ -Xrunhprof:heap=sites,cpu=samples\
+ "$@"
