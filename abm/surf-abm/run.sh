4c700ad20e5b78849424d3dec58d8759e9a8c2e1
Nick Malleson <n.s.malleson@leeds.ac.uk>
2017-07-31 09:33:13 +0100

    added info about how to run on the command line

--- abm/surf-abm/run.sh
+++ abm/surf-abm/run.sh
@@ -1,3 +1,7 @@
+# Run the model.
+# Command-line options that are defined by MASON can be passed to configure the run. 
+# E.g. for a simulation of 5000 iterations, summarising info every 1000 iterations, and a seed of 1:
+#  ./run.sh -for 5000 -time 1000 -seed 1
 env JAVA_OPTS="-Xmx4G" \
 scala -cp ../resources/mason/mason/build/:\
 ../resources/mason/contrib/geomason/build/:\
