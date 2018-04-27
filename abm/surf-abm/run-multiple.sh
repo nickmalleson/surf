6e9d647fd70b0bf86d2e630afb663c5902af1914
Nick Malleson <n.s.malleson@leeds.ac.uk>
2018-04-27 12:40:07 +0100

    added script to allow model to be run N times in one call

---
+++ abm/surf-abm/run-multiple.sh
@@ -0,0 +1,19 @@
+# Run the model a number of times, one after the other. Takes the number of times to run as the only
+# argument. No command-line options to MASON are allowed, these need to be set in the commands
+# below.
+
+N=$1
+echo "Will run $1 times"
+for i in `seq 1 1 $N`
+do
+echo "RUNNING $i"
+env JAVA_OPTS="-Xmx4G" \
+scala -cp ../resources/mason/mason/build/:\
+../resources/mason/contrib/geomason/build/:\
+../resources/mason_resources/*:\
+./lib/*:\
+./build/:\
+../resources/config/classes/\
+ surf.abm.main.SurfABM -for 1000 -time 100 -seed 1
+done
+
