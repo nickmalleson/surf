5ed82899efd0d246271cfb0bdb5f9c6a877d9d0e
Nick Malleson <n.s.malleson@leeds.ac.uk>
2018-10-22 17:09:53 +0100

    changed run-multiple script to do proper batch runs

--- abm/surf-abm/run-multiple.sh
+++ abm/surf-abm/run-multiple.sh
@@ -1,12 +1,15 @@
 # Run the model a number of times, one after the other. Takes the number of times to run as the only
 # argument. No command-line options to MASON are allowed, these need to be set in the commands
-# below.
+# below. Runs in batch jobs to CORES cores (will only do CORES models at once, as per second
+# suggestion here: https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop)
 
-N=$1
-echo "Will run $1 times"
-for i in `seq 1 1 $N`
-do
-echo "RUNNING $i"
+CORES=8 # Number of cores to use
+N=$1 # total number of times to run the model (command-line argument)
+echo "Will run $1 times in $CORES cores"
+
+for i in `seq 1 1 $1`; do
+    ((x=x%CORES)); ((x++==0)) && wait
+    echo "RUNNING $i"
 env JAVA_OPTS="-Xmx4G" \
 scala -cp ../resources/mason/mason/build/:\
 ../resources/mason/contrib/geomason/build/:\
@@ -14,7 +17,6 @@ scala -cp ../resources/mason/mason/build/:\
 ./lib/*:\
 ./build/:\
 ../resources/config/classes/\
- surf.abm.main.SurfABM -for 2000 -time 500 -seed 1 &
-sleep 5;
+ surf.abm.main.SurfABM -for 4032 -time 500 -seed $i &
+    sleep 5;
 done
-
